2e0cc673e464fd792012ab469505e139f393e0d2
Michael Gardiner <michaelg4963@gmail.com>
2020-07-26 23:49:26 +0100

    Progress

--- deploy-heroku.bat
+++ deploy-heroku.bat
@@ -1,3 +1,7 @@
-heroku login
-heroku git:remote -a tiltnpour
+Rem  heroku login
+Rem  heroku git:remote -a tiltnpour
+git add .
+
+git commit -m "Progress"
+
 git subtree push --prefix server heroku master
