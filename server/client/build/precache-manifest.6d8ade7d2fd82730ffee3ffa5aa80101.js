261332ba3337782b358a707b2d52e408426fff13
Michael Gardiner <michaelg4963@gmail.com>
2020-08-02 18:44:28 +0100

    Heroku deploy commit

---
+++ server/client/build/precache-manifest.6d8ade7d2fd82730ffee3ffa5aa80101.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b12de0450633b2d6ef47f4a5269d7df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08ec7c84c275af079716",
+    "url": "/static/js/2.4e9ed6d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4e9ed6d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e0ea1703e02cf3d7f4",
+    "url": "/static/js/main.c6d778a2.chunk.js"
+  },
+  {
+    "revision": "eacdcc3c89caa39e8ec1",
+    "url": "/static/js/runtime-main.1a837c63.js"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/instagram.0e84d592.png"
+  },
+  {
+    "revision": "4330f3006f344a8e16029c2ccbcc3257",
+    "url": "/static/media/link.4330f300.svg"
+  }
+]);
